acb44d239c1188013829753ed404b6c73a51bdfd
mops <banbanbanbanbanban5@gmail.com>
2025-03-06 18:49:48 +0300

    2 	new file:   .idea/misc.xml 	modified:   .idea/workspace.xml 	new file:   images/[SW.BAND] alien.png 	new file:   images/[SW.BAND] fighter.png 	new file:   images/[SW.BAND] rocket.png 	new file:   main.py 	new file:   rectangle.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+
+from rectangle import screen_width, screen_height
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
